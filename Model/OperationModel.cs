a0e89c89858d664736bce19b4d6fe50440b2e4dc
pinkeparu <pinkeparu@live.com>
2016-04-28 10:55:51 +0530

    Simple Calculator

---
+++ Model/OperationModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CalcProgram
+{
+    class OperationModel
+    {
+        private double firstNumber;
+        
+        public double FirstNumber
+        {
+            get { return firstNumber; }
+            set { firstNumber = value; }
+        }
+
+        private double secondNumber;
+
+        public double SecondNumber
+        {
+            get { return secondNumber; }
+            set { secondNumber = value; }
+        }
+
+        private double result;
+
+        public double Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+
+        public void Add()
+        {
+            Result = FirstNumber + SecondNumber;
+        }
+
+        public void Subtract()
+        {
+            Result = FirstNumber - SecondNumber;
+        }
+
+        public void Multiply()
+        {
+            Result = SecondNumber * FirstNumber;
+        }
+
+        public void Divide()
+        {
+            Result = FirstNumber / SecondNumber;
+        }
+    }
+}
---
+++ ViewModel/Operations.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CalcProgram.ViewModel
+{
+    class OperationsViewModel : NotifyClass, IDataErrorInfo
+    {
+        OperationModel objOperationModel = new OperationModel();
+        OperationCommand objOperationCommand;// = new OperationCommand();
+
+        public OperationsViewModel()
+        {
+            objOperationCommand = new OperationCommand(Calculate, isValid);
+        }
+
+        bool isValid()
+        {
+            int Tempresult=0;
+            if (!IsValidNumber(input1, out Tempresult))
+            {
+                return false;
+            }
+
+            if (!IsValidNumber(input2, out Tempresult))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        bool validateInput(string value)
+        {
+            int result;
+
+            if (int.TryParse(value.ToString(), out result))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public ICommand cmd
+        {
+            get { return objOperationCommand; }
+        }
+
+        string input1;
+        public string txtInput1
+        {
+            get
+            {
+                return input1;
+            }
+            set
+            {
+                input1 = value;
+            }
+        }
+
+        string input2;
+        public string txtInput2
+        {
+            get
+            {
+                return input2;
+            }
+            set
+            {
+                input2 = value;
+            }
+        }
+
+        public string Result
+        {
+            get
+            {
+                return objOperationModel.Result.ToString();
+            }
+            set
+            {
+                objOperationModel.Result = Convert.ToDouble(value);
+            }
+        }
+
+        public void Calculate(object obj)
+        {
+            switch (obj.ToString())
+            {
+                case "Add":
+                    objOperationModel.Add();
+                    break;
+                case "Subtract":
+                    objOperationModel.Subtract();
+                    break;
+                case "Multiply":
+                    objOperationModel.Multiply();
+                    break;
+                case "Divide":
+                    objOperationModel.Divide();
+                    break;
+            }
+
+            PropertyChangedMethod("Result");
+        }        
+
+        #region DataErrorInfo Member
+
+        public string Error
+        {
+            get { return null; }
+        }
+
+        public string this[string propertyName]
+        {
+            get
+            {
+                int result;
+                if (propertyName == "txtInput1")
+                {
+                    objOperationCommand.Refresh();
+                    if (!IsValidNumber(input1, out result))
+                    {
+                        objOperationModel.FirstNumber = 0;                       
+                        return "Input must be number";
+                    }
+                    else
+                    {
+                        objOperationModel.FirstNumber = result;
+                    }
+                }
+
+                int result2;
+                if (propertyName == "txtInput2")
+                {
+                    objOperationCommand.Refresh();
+                    if (!IsValidNumber(input2, out result2))
+                    {
+                        objOperationModel.SecondNumber = 0;                       
+                        return "Input must be number";
+                    }
+                    else
+                    {
+                        objOperationModel.SecondNumber = result2;
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        public bool IsValidNumber(string input, out int output)
+        {
+            if (int.TryParse(input, out output))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
+    }
+
+    class OperationCommand : ICommand
+    {
+        Action<object> whatToExecute;
+        Func<bool> whenToExecute;
+
+        public OperationCommand(Action<object> what, Func<bool> when)
+        {
+            whatToExecute = what;
+            whenToExecute = when;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return whenToExecute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            whatToExecute(parameter);
+        }
+
+        public void Refresh()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
